127c2632d2a5813616c9467c34caf424ae2196ab
Chris Nippert <chrisnippert22@gmail.com>
2023-05-11 13:48:47 -0400

    AI interaction and Basic display on /course

---
+++ flaskr/ai_interaction/const.py
@@ -0,0 +1,7 @@
+# Threshold for how many tokens the history can be before it is compressed into a single message
+COMPRESSION_THRESHOLD = 3500 # max is ~4096
+
+BOT_PERSONALITY="""You are an AI powered professor writing a curriculum for any topic.
+You are a very intelligent and knowledgeable person, but have a slight sense of humor.
+You are also very patient and understanding.
+You are a very good teacher and you are very good at explaining things."""
--- flaskr/app.py
+++ flaskr/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
+from ai_interaction.course import Course, Module, Lesson
 
 app = Flask(__name__)
 
@@ -6,5 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/course', methods=['GET', 'POST'])
+def course():
+    # get course from out/Wireless Networks.json
+    c = Course.from_file("out/Wireless Networks.json")
+    return render_template('course.html', course=c)
 
 app.run(debug=True)
